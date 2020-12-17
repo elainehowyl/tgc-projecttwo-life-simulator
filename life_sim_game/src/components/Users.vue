ebc458dbb0d8dd648b9a68a1b4e7396d54d437ae
elainehowyl <elaine_0110@hotmail.com>
2020-12-17 09:40:28 +0000

    managed to display database

---
+++ life_sim_game/src/components/Users.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>All Users Info</h2>
+        <button v-on:click="loadUsers">Display All Users</button>
+        <div v-for="u in users" v-bind:key="u._id">
+            <h4>Username: {{u.username}}</h4>
+            <h4>Password: {{u.password}}</h4>
+            <h4>Email: {{u.email}}</h4>
+            <h4>Display Name: {{u.displayname}}</h4>
+            <h4>Gender: {{u.gender}}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    // created: async function(){
+    //     let response = await axios.get('https://3001-b95582b4-ae68-4f74-ad61-58cb4afbe719.ws-us03.gitpod.io/users')
+    //     this.users = response.data
+    // },
+    data:function(){
+        return{
+            'users':[]
+       }
+    },
+    methods:{
+        loadUsers: async function(){
+            let response=await axios.get('https://3001-b95582b4-ae68-4f74-ad61-58cb4afbe719.ws-us03.gitpod.io/users')
+            this.users = response.data
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- life_sim_game/src/main.js
+++ life_sim_game/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Users from './components/Users'
 
 Vue.config.productionTip = false
 
+const routes = [
+    {path:'/users', component:Users}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+Vue.use(VueRouter)
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')

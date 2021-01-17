651967c0c65c6c254cc0e62beb87b415cffb2c29
elainehowyl <elaine_0110@hotmail.com>
2021-01-17 08:10:18 +0000

    completed form validation

--- life_sim_game/src/components/sidebar/SideButtons.vue
+++ life_sim_game/src/components/sidebar/SideButtons.vue
@@ -13,26 +13,26 @@
               <label> Username: </label>
               <b-form-input type="text" size="sm" v-model="registerUsername"/>
               <p v-if="registeredUsername===true" v-bind:style="{color:'red', fontSize:'16px'}">Username is taken.</p>
-              <p v-else></p>
               <p v-if="usernameNoInput===true" v-bind:style="{color:'red', fontSize:'16px'}">Please enter a username.</p>
-              <p v-else></p>
+              <p v-if="registeredUsername===false && usernameNoInput===false"></p>
               <label> Password: </label>
               <b-form-input type="password" size="sm" v-model="registerPassword"/>
-              <p v-if="registeredUsername===true" v-bind:style="{color:'red', fontSize:'16px'}">Password must be at least 8 characters.</p>
+              <p v-if="passwordTooShort===true" v-bind:style="{color:'red', fontSize:'16px'}">Password must be at least 8 characters.</p>
               <label> Re-enter Password: </label>
               <b-form-input type="password" size="sm" v-model="registerPasswordRe"/>
-              <p v-if="registeredUsername===true" v-bind:style="{color:'red', fontSize:'16px'}">Password does not match.</p>
+              <p v-if="passwordMismatch===true" v-bind:style="{color:'red', fontSize:'16px'}">Password does not match.</p>
               <label> Email: </label>
               <b-form-input type="email" size="sm" v-model="registerEmail"/>
-              <p v-if="registeredUsername===true" v-bind:style="{color:'red', fontSize:'16px'}">Please enter a valid email.</p>
+              <p v-if="emailInvalid===true" v-bind:style="{color:'red', fontSize:'16px'}">Please enter a valid email.</p>
               <label> Display Name: </label>
               <b-form-input type="text" size="sm" v-model="registerDisplayName"/>
-              <p v-if="registeredUsername===true" v-bind:style="{color:'red', fontSize:'16px'}">Please enter a display name.</p>
+              <p v-if="displayNameNoInput===true" v-bind:style="{color:'red', fontSize:'16px'}">Please enter a display name.</p>
              </div>
              <div>
                 <label>Gender: </label>
                 <b-form-radio v-model="selectGender" value="female">Female</b-form-radio>
                 <b-form-radio v-model="selectGender" value="male">Male</b-form-radio>
+                <p v-if="genderNotSelected===true" v-bind:style="{color:'red', fontSize:'16px'}">Please select a gender.</p>
              </div>
              <template #modal-footer="{cancel}">
                 <b-button @click="newRegister" size="sm" variant="success">Register</b-button>
@@ -56,11 +56,12 @@ export default {
             registerDisplayName:'',
             selectGender:'',
             registeredUsername:false,
-            usernameNoInput:null,
-            passwordTooShort:null,
-            passwordMismatch:null,
-            emailInvalid:null,
-            displayNameNoInput:null
+            usernameNoInput:false,
+            passwordTooShort:false,
+            passwordMismatch:false,
+            emailInvalid:false,
+            displayNameNoInput:false,
+            genderNotSelected:false,
         }
     },
     methods:{
@@ -83,7 +84,8 @@ export default {
                 this.passwordMismatch = this.registerPasswordRe !== this.registerPassword ? true:false
                 this.emailInvalid = !this.registerEmail.includes('@') && !this.registerEmail.includes('.') ? true:false
                 this.displayNameNoInput = this.registerDisplayName.length === 0 ? true:false
-                if(!this.usernameNoInput && !this.passwordTooShort && !this.passwordMismatch && !this.emailInvalid && !this.displayNameNoInput){
+                this.genderNotSelected = !this.selectGender.checked ? true:false
+                if(!this.usernameNoInput && !this.passwordTooShort && !this.passwordMismatch && !this.emailInvalid && !this.displayNameNoInput && !this.genderNotSelected){
                     formValid = true
                 }
                 if(formValid){
